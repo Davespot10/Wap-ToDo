36e8ff6fe3762a8baf71a1a4cb0939dd3c23e7b3
Davespot10 <davespot10@gmail.com>
2022-08-08 03:42:26 -0500

    ui updated

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'MerDevTodo' });
+  res.render('index.ejs', { title: 'MerDevTodo' });
 });
 
 module.exports = router;
