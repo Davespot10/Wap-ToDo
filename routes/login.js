9a75996cfd0bba934bef6462a9e283586e1eb37e
Dawit Meketa <dawitsoft@gmail.com>
2022-08-08 21:01:43 -0500

    Revert "Login updated"
    This reverts commit 1c2b89dced0f4fca963209e88d10f7c4b8230f9c.

--- routes/login.js
+++ routes/login.js
@@ -1,27 +1,15 @@
-const express = require('express');
-const user = require("../models/user");
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
 
 /* GET home page. */
 router.get('/login', function(req, res, next) {
     res.render("login.ejs");
 })
-router.get("/signup", (req,res)=>{
+router.get("/signup",(req,res)=>{
     res.render("signup.ejs");
 })
-router.post("/postlogin",async (req, res) => {
-
-    let data = await user.find()
-
-    if (data.some(user => user.userId === req.body.userN && user.password === req.body.pass)) {
-
-        res.cookie("currentUser", req.body.userN+","+req.body.pass)
-
-        res.redirect("/homepage")
-    } else {
-        res.redirect("/login")
-    }
-
+router.post("/postlogin",(req,res)=>{
+    res.redirect("/homepage")
 })
 router.post("/postsignup",(req,res)=>{
     res.redirect("/login")
