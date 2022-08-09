b04c09eac73ba5328009dde77db74313b38b4164
Merhatsidk <merhatsidk29@gmail.com>
2022-08-09 02:52:18 -0500

    put method for category added

--- routes/login.js
+++ routes/login.js
@@ -3,17 +3,30 @@ const user = require("../models/user");
 const router = express.Router();
 
 /* GET home page. */
-router.get('/login', function(req, res, next) {
-    res.render("login.ejs");
-})
-router.get("/signup",(req,res)=>{
-    res.render("signup.ejs");
-})
-router.post("/postlogin",(req,res)=>{
-    res.redirect("/homepage")
-})
-router.post("/postsignup",(req,res)=>{
-    res.redirect("/login")
-})
+router.get("/login", function (req, res, next) {
+  res.render("login.ejs");
+});
+router.get("/signup", (req, res) => {
+  res.render("signup.ejs");
+});
+router.post("/postlogin", async (req, res) => {
+  let data = await user.find();
+
+  if (
+    data.some(
+      (user) =>
+        user.userId === req.body.userName && user.password === req.body.password
+    )
+  ) {
+    res.cookie("currentUser", req.body.userName + "," + req.body.password);
+
+    res.redirect("/homepage");
+  } else {
+    res.redirect("/login");
+  }
+});
+router.post("/postsignup", (req, res) => {
+  res.redirect("/login");
+});
 
 module.exports = router;
