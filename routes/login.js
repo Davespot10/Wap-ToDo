289252684dfce24fd6cc91f0bc09d8e545751eeb
Davespot10 <davespot10@gmail.com>
2022-08-10 17:20:04 -0500

    worknig login page update

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const user = require("../models/user");
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const TOKEN_SECRET= "aff4c949636f59083476a2ec64ad167b462eb7abeb2cf6d11bfc77b2e970931db96035dba90e33b800c0bc612ec0f6fbaccec4240d01180b31942f222229a844";
-
+const jwt = require("jsonwebtoken");
+const TOKEN_SECRET =
+  "aff4c949636f59083476a2ec64ad167b462eb7abeb2cf6d11bfc77b2e970931db96035dba90e33b800c0bc612ec0f6fbaccec4240d01180b31942f222229a844";
 
 /* GET home page. */
 router.get("/login", function (req, res, next) {
@@ -13,46 +13,42 @@ router.get("/signup", (req, res) => {
   res.render("signup.ejs");
 });
 router.post("/postlogin", async (req, res) => {
-  console.log("incoming",req.body);
+  console.log("incoming", req.body);
   let data = await user.find();
 
   if (
-      data.some(
-          (user) =>
-              user.userId === req.body.username && user.password === req.body.password
-      )
+    data.some(
+      (user) =>
+        user.userId === req.body.username && user.password === req.body.password
+    )
   ) {
+    let userToken;
 
-      let userToken;
-
-      try {
-          //Creating jwt token
-          userToken = jwt.sign(
-              {userId: req.body.username, password: req.body.password},
-              TOKEN_SECRET,
-              {expiresIn: "1h"}
-          );
-
-      } catch (err) {
-          console.log(err);
-          const error = new Error("Error! Something went wrong in token creation");
-         res.send(error)
-      }
-      console.log(userToken);
-      res.cookie("currentUser", userToken);
-      res.end('success')
-    // res.redirect("/homepage");
-
+    try {
+      //Creating jwt token
+      userToken = jwt.sign(
+        { userId: req.body.username, password: req.body.password },
+        TOKEN_SECRET,
+        { expiresIn: "1h" }
+      );
+    } catch (err) {
+      console.log(err);
+      const error = new Error("Error! Something went wrong in token creation");
+      res.send(error);
+    }
+    console.log(userToken);
+    res.cookie("currentUser", userToken);
+    // res.end('success')
+    res.redirect("/homepage");
   } else {
-
     res.redirect("/login");
   }
 });
 
-router.post('/postLogout',(req,res)=>{
-    res.clearCookie("currentUser");
-    res.redirect('/login');
-})
+router.post("/postLogout", (req, res) => {
+  res.clearCookie("currentUser");
+  res.redirect("/login");
+});
 router.post("/postsignup", (req, res) => {
   res.redirect("/users/addUser");
 });
