e0016962ec31b67e828552ee6bdb281dd3dc9c38
Davespot10 <davespot10@gmail.com>
2022-08-10 10:14:21 -0500

    fixed spelling issues

--- test.js
+++ test.js
@@ -1,10 +1,6 @@
-
-
-
-
 // let count=0;
 // for(let i=0;i<todo.length;i++){
-//     if(todo.length>0&&todo[i].catagory=="sport"){
+//     if(todo.length>0&&todo[i].category=="sport"){
 //         count++;
 //         todo.forEach(todo=>{
 //                 //  <h2 class="title"> "Title: "<%= todo.title %> </h2>
@@ -20,19 +16,18 @@
 
 // }
 
-
 //  <% if(todo.length>0){ %>
-//    <% for(let i=0;i<todo.length;i++){ %>  
-//      <% if(todo[i].catagory=="sport"){ %>    
+//    <% for(let i=0;i<todo.length;i++){ %>
+//      <% if(todo[i].category=="sport"){ %>
 //             <% todo.forEach(todo=>{%>
 //                 <h2 class="title"><%= todo.title %> </h2>
 //                 <h3 class="description"><%= todo.description %></h3>
 //                 <p class="plannedDate"><%= todo.plannedDate %></p>
-//            <% })   %> 
+//            <% })   %>
 
-//        <% } %>  
-    
-//    <% } %>    
-//    <%  }else{ %> 
+//        <% } %>
+
+//    <% } %>
+//    <%  }else{ %>
 //         <h2> SORRY!!There is on task to diplay...</h2>
 //   <% } %>
