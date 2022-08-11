35be94797b6b02375e8e512e0bc99694353646c8
Davespot10 <davespot10@gmail.com>
2022-08-11 00:49:48 -0500

    UI Updated

--- test.js
+++ test.js
@@ -31,3 +31,26 @@
 //    <%  }else{ %>
 //         <h2> SORRY!!There is on task to diplay...</h2>
 //   <% } %>
+//Edit.js Delated
+
+{/* <h2 style="text-align: center;"> <i><b>Welcome to TODO APP</b></i> </h2>
+
+<h2 style="text-align: center;"> <i><b>Planning means looking ahead!!!</b></i> </h2>
+<div id="middlebody">
+    <h2>Title</h2><input id="title" type="text" name="title" value="<%= todo.title%>">
+
+    <h2>category</h2>
+    <select id="category" value="<%= todo.category %>" name="category" id="">
+        <option value="sport">sport</option>
+        <option value="study">study</option>
+        <option value="shopping">shopping</option>
+        <option value="travel">travel</option>
+    </select>
+    <h2>Due Date</h2> <input name="due_date" id="due_date" type="due_date" value="<%= todo.due_date %>">
+    <br>
+    <h2>Discription</h2>
+    <textarea placeholder="Insert Your Task Discreption here" name="description" id="description" cols="60"
+        rows="10"><%= todo.description %></textarea>
+    <br>
+    <button onclick="update()" id="submitbutton">Submit</button>
+</div> */}
