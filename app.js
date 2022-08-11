f05da46c6e157d269134f7a462e5799e7d3194fb
Davespot10 <davespot10@gmail.com>
2022-08-10 20:16:10 -0500

    ui done

--- app.js
+++ app.js
@@ -1,40 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const fetch = require('node-fetch')
-const authRoute= require('./routes/auth')
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const fetch = require("node-fetch");
+const authRoute = require("./routes/auth");
 const mongoose = require("mongoose");
-var user = require('./models/user');
-let loginRoute=require("./routes/login")
-let hompageRoute=require("./routes/homepage")
-let alltaskRoute=require("./routes/alltask")
-let complatedtaskRoute=require("./routes/complatedtask")
-let pendingtaskRoute=require("./routes/pendingtask")
-let delatedtaskRoute=require("./routes/delatedtask")
-let addTaskRoute=require("./routes/addtask")
+var user = require("./models/user");
+let loginRoute = require("./routes/login");
+let hompageRoute = require("./routes/homepage");
+let alltaskRoute = require("./routes/alltask");
+let complatedtaskRoute = require("./routes/complatedtask");
+let pendingtaskRoute = require("./routes/pendingtask");
+let deletedtaskRoute = require("./routes/deletedtask");
+let addTaskRoute = require("./routes/addtask");
 
-const cors = require('cors')
+const cors = require("cors");
 
+let editRoute = require("./routes/edit");
 
-let editRoute = require('./routes/edit')
-
-
-
-
-
-
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var itemRouter = require('./routes/item');
-var todoRouter = require('./routes/todo');
-
-
-
-
-
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var itemRouter = require("./routes/item");
+var todoRouter = require("./routes/todo");
 
 var app = express();
 
@@ -50,33 +38,35 @@ mongoose
   });
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use('/css', express.static('public/stylesheets'));
-app.use(cors({
-  origin: '*'
-}))
+app.use("/css", express.static("public/stylesheets"));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/todo',todoRouter);
-app.use('/users', usersRouter);
-app.use('/item', itemRouter);
-app.use(loginRoute)
+app.use("/", indexRouter);
+app.use("/todo", todoRouter);
+app.use("/users", usersRouter);
+app.use("/item", itemRouter);
+app.use(loginRoute);
 // app.use('/*',authRoute)
-app.use(hompageRoute)
-app.use(alltaskRoute)
-app.use(complatedtaskRoute)
-app.use(pendingtaskRoute)
-app.use(delatedtaskRoute)
-app.use(addTaskRoute)
-app.use(editRoute)
+app.use(hompageRoute);
+app.use(alltaskRoute);
+app.use(complatedtaskRoute);
+app.use(pendingtaskRoute);
+app.use(deletedtaskRoute);
+app.use(addTaskRoute);
+app.use(editRoute);
 
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
@@ -84,14 +74,14 @@ app.use(editRoute)
 // });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
---
+++ routes/deletedtask.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var router = express.Router();
+const Todo = require("../models/todo");
+router.get("/deletedtask", function (req, res, next) {
+  Todo.find()
+    .sort({ createdTime: -1 })
+    .then((result) => {
+      res.render("deletedtask.ejs", { title: "deleted", todo: result });
+    })
+    .catch(() => {
+      console.log(err);
+    });
+});
+
+module.exports = router;
